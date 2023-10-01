91353147cf57279e9368b896fcf04ad99519c3d5
omerfdev <omerfdev@gmail.com>
2023-10-01 23:02:38 +0300

    .

---
+++ RegisterLoginPage/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RegisterLoginPage.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
